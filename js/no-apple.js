b32ef546945080c271dbc734776f74c567045ac6
XM9G <xm9g@outlook.com>
2023-09-03 20:57:40 +1000

    no ipads!

--- js/no-apple.js
+++ js/no-apple.js
@@ -2,11 +2,11 @@
 function isAppleDevice() {
     const userAgent = navigator.userAgent;
     console.log("User Agent:", userAgent); // Log the user agent to the console
-    return /iPhone|iPad|Macintosh/.test(userAgent);
+    return /iPhone|iPad|Mac/.test(userAgent);
 }
 
 // Redirect if the page is opened on an Apple device
 if (isAppleDevice()) {
     // Change 'destination-url' to the URL where you want to redirect
-    window.location.href = 'https://example.com'; // Replace with your destination URL
+    window.location.href = 'No-Apple.html'; // Replace with your destination URL
 }
