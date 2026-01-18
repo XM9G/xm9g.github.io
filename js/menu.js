c23889e8ea939cdaad6651d72d4a8ae8214735cf
Xm9G <xm9g@outlook.com>
2026-01-18 16:20:17 +1100

    updated site

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,17 @@
 // Create a header element
 const header = document.createElement('header');
-header.innerHTML = '<div class="menu"><ul><li><a href="/index.html"><i class="fa-solid fa-house"></i> Home</a></li><li><a href="https://victorianrailphotos.com"><i class="fa-solid fa-train-subway"></i> Railway Photos</a></li><li><a href="/longestcall/index.html"><i class="fa-solid fa-phone"></i> Longest Call</a></li><li><a href="/specs.html"><i class="fa-solid fa-desktop"></i> PC Specs</a></li><li><a href="/new-years-counter/"><i class="fa-solid fa-clock"></i> New Years Countdown</a></li><li><a href="/pp/index.html"><i class="fa-regular fa-comment"></i> Professional Pronunciation</a></li></ul></div>';
+header.innerHTML = `
+    <div class="menu">
+        <ul>
+            <li><a href="/index.html"><i class="fa-solid fa-house"></i> Home</a></li>
+            <li><a href="https://victorianrailphotos.com"><i class="fa-solid fa-train-subway"></i> Railway Photos</a></li>
+            <li><a href="/longestcall/index.html"><i class="fa-solid fa-phone"></i> Longest Call</a></li>
+            <li><a href="/specs.html"><i class="fa-solid fa-desktop"></i> PC Specs</a></li>
+            <li><a href="/new-years-counter/"><i class="fa-solid fa-clock"></i> New Years Countdown</a></li>
+            <li><a href="/pp/index.html"><i class="fa-regular fa-comment"></i> Professional Pronunciation</a></li>
+        </ul>
+    </div>
+`;
 
 // Append the header element to the <body> of each page
 document.body.appendChild(header);
-
